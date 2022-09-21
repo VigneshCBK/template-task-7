9b18b5a259676c2f59c3f80b73042ae30bd5932d
VigneshCBK <vickykasthu@gmail.com>
2022-09-21 17:12:05 +0530

    update

--- docs/main.js
+++
@@ -1,9 +0,0 @@
-function getMonday(d) {
-    d = new Date(d);
-    var day = d.getDay()
-    date = d.getDate() - day + (day == 0 ? -6 : 1);
-    return new Date(d.setDate(date));
-}
-
-getMonday(("2022-10-2"));
-
